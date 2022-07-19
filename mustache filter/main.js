873f10e3e6b033c1857757bd2b51d78758a8dbcc
SuryanshSingh123 <99817718+SuryanshSingh123@users.noreply.github.com>
2022-07-19 18:13:40 +0530

    Add files via upload

--- mustache filter/main.js
+++ mustache filter/main.js
@@ -4,12 +4,29 @@ function preload(){
 function setup(){
    Canvas = createCanvas(400,400);
    Canvas.center();
+   video = createCapture(VIDEO);
+   video.size(400,400);
+   video.hide();
+
+   poseNet=ml5.poseNet(video, modelLoaded);
+   poseNet.on('pose', gotPoses);
 }
 
 function draw(){
-
+image(video ,0,0,400,400);
 }
-
+function modelLoaded(){
+    console.log('Posenet is Initialized');
+}
 function take_snapshot(){
     save('mustache.jpeg');
+}
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x="+ results[0].pose.nose.x);
+        console.log("nose y="+ results[0].pose.nose.y);
+    }
 }
